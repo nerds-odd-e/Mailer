2c04f55e20a33515a5758a8428caca2547450fc0
adityapan <pan.aditya93@gmail.com>
2018-03-21 15:43:41 +0800

    add db for test, update config connection string

--- DbCreation.ps1
+++ DbCreation.ps1
@@ -5,36 +5,47 @@ $con = New-Object Data.SqlClient.SqlConnection;
 $con.ConnectionString = "Data Source=.\SQLEXPRESS;Initial Catalog=master;Integrated Security=True;";
 $con.Open();
 
-# Select-Statement for AD group logins
-$sql = "SELECT name
-        FROM sys.databases
-        WHERE name = '$dbname';";
-
-# New command and reader.
-$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
-$rd = $cmd.ExecuteReader();
-if ($rd.Read())
-{	
-    $rd.Close();
-    $rd.Dispose();
-    $sql = "DROP DATABASE [$dbname]"
-    $cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
-    $cmd.ExecuteNonQuery();	
-	Write-Host "Database $dbname dropped";
-}
-
-$rd.Close();
-$rd.Dispose();
 
 # Create the database.
-$sql = "CREATE DATABASE [$dbname];"
+$sql = "IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = '$dbname')
+    BEGIN
+        CREATE DATABASE [$dbname]
+    END;"
 $cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
 $cmd.ExecuteNonQuery();		
 Write-Host "Database $dbname is created!";
-$sql = "use $dbname  CREATE Table Contact (ID int IDENTITY(1,1) PRIMARY KEY, Email VARCHAR(50) not null );";
+$sql = "
+use $dbname  
+IF  NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Contact')
+    BEGIN
+        CREATE Table Contact (ID int IDENTITY(1,1) PRIMARY KEY, Email VARCHAR(50) not null);
+    END;
+";
 $cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
 $cmd.ExecuteNonQuery();	
 
+[String]$dbname = "MailerDBTest";
+$sql = "IF  NOT EXISTS (SELECT * FROM sys.databases WHERE name = '$dbname')
+    BEGIN
+        CREATE DATABASE [$dbname]
+    END
+    ELSE
+    BEGIN
+    	DROP DATABASE [$dbname]
+    	CREATE DATABASE [$dbname]
+    END;"
+$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+$cmd.ExecuteNonQuery();		
+Write-Host "Test Database $dbname is created!";
+
+$sql = "
+use $dbname  
+IF  NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Contact')
+    BEGIN
+        CREATE Table Contact (ID int IDENTITY(1,1) PRIMARY KEY, Email VARCHAR(50) not null);
+    END;";
+$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+$cmd.ExecuteNonQuery();	
 
 # Close & Clear all objects.
 $cmd.Dispose();
