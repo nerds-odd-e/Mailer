a52905e80ef73f8a1f8a9019c776c88ba49bcec7
b7luestar <b7luestar@gmail.com>
2018-03-22 16:18:19 +0800

    added courses

--- DbCreation.ps1
+++ DbCreation.ps1
@@ -47,7 +47,12 @@ use $dbname
 IF  NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Contact')
     BEGIN
         CREATE Table Contact (ID int IDENTITY(1,1) PRIMARY KEY, Email VARCHAR(50) not null);
-    END;";
+    END;
+IF  NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Courses')
+    BEGIN
+        create table courses (    Id int Identity primary key, CourseName varchar(255) not null, StartDate smalldatetime not null, EndDate smalldatetime not null)
+    END;
+    ";
 $cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
 $cmd.ExecuteNonQuery();	
 
