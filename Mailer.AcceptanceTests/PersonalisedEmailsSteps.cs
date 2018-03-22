8534c5cd55aeedc8396f58cc8118cf1cd039029a
adityapan <pan.aditya93@gmail.com>
2018-03-22 15:51:55 +0800

    Add PersonalisedEmail Step cs File

---
+++ Mailer.AcceptanceTests/PersonalisedEmailsSteps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Mailer.AcceptanceTests
+{
+    [Binding]
+    public class PersonalisedEmailsSteps
+    {
+        [Given(@"(.*) (.*)'s email address (.*) is registered in system")]
+        public void GivenXmanLogoSEmailAddressLxGmail_ComIsRegisteredInSystem(string firstName, string lastName, string email)
+        {
+
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Recepient's email starts with (.*)")]
+        public void ThenRecepientSEmailStartsWith(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
