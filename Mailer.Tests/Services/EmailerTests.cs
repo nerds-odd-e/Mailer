fe1767d37d02bd1da80a0a1539b252da35119650
bluestar <b7luestar@gmail.com>
2018-03-23 10:42:25 +0800

    Refactor EmailerTests

--- Mailer.Tests/Services/EmailerTests.cs
+++ Mailer.Tests/Services/EmailerTests.cs
@@ -8,27 +8,41 @@ namespace Mailer.Tests.Services
     [TestFixture]
     public class EmailerTests
     {
-        [Test]
-        public void TestEmailer()
+        private ISmtpClient _smtpClient;
+        private Emailer _emailer;
+
+        [SetUp]
+        public void SetUp()
         {
-            var client = Substitute.For<ISmtpClient>();
-            var emailer = new Emailer(client);
-            var recipientList = new List<string> { "test1@gmail.com", "test2@gmail.com" };
-            var subject = "this is a test email.";
-            var body = "this is my test email body";
-            var fromAddress = "myodde@gmail.com";
+            _smtpClient = Substitute.For<ISmtpClient>();
+            _emailer = new Emailer(_smtpClient);
+        }
 
-            var actualEmails = emailer.ConstructEmails(recipientList);
-            Assert.AreEqual(2, actualEmails.Count);
-            Assert.IsTrue(actualEmails[0].From.Address.Contains(fromAddress));
+        [Test]
+        public void DoNotSend_WhenNoRecipient()
+        {
+            var recipientList = new List<string>();
+            var actualEmails = _emailer.ConstructEmails(recipientList);
+            Assert.AreEqual(0, actualEmails.Count);
+        }
+
+        [Test]
+        public void SendOneEmail_WhenOnlyOneRecipient()
+        {
+            var recipientList = new List<string>{ "test1@gmail.com" };
+            var actualEmails = _emailer.ConstructEmails(recipientList);
+            Assert.AreEqual(1, actualEmails.Count);
             Assert.AreEqual("test1@gmail.com", actualEmails[0].To[0].Address);
-            Assert.AreEqual(subject, actualEmails[0].Subject);
-            Assert.AreEqual(body, actualEmails[0].Body);
+        }
 
-            Assert.IsTrue(actualEmails[1].From.Address.Contains(fromAddress));
+        [Test]
+        public void SendMultiple_WhenMultipleRecipient()
+        {
+            var recipientList = new List<string> {"test1@gmail.com", "test2@gmail.com"};
+            var actualEmails = _emailer.ConstructEmails(recipientList);
+            Assert.AreEqual(2, actualEmails.Count);
+            Assert.AreEqual("test1@gmail.com", actualEmails[0].To[0].Address);
             Assert.AreEqual("test2@gmail.com", actualEmails[1].To[0].Address);
-            Assert.AreEqual(subject, actualEmails[1].Subject);
-            Assert.AreEqual(body, actualEmails[1].Body);
         }
     }
 
